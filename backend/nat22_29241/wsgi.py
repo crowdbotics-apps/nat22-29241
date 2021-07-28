42d92de4b39a9886511f407e96c99a9598064456
Crowdbotics <team@crowdbotics.com>
2021-07-28 05:24:58 +0000

    Update models

--- backend/nat22_29241/wsgi.py
+++ backend/nat22_29241/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nat22_29241.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nat22_29241.settings")
 
 application = get_wsgi_application()
