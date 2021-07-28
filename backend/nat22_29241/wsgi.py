fcdcd64c8cca730589a983bcf7de93056660aa8c
Crowdbotics <team@crowdbotics.com>
2021-07-28 05:13:24 +0000

    Initial commit.

---
+++ backend/nat22_29241/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nat22_29241 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nat22_29241.settings')
+
+application = get_wsgi_application()
